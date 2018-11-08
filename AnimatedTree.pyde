742c5ab1ff48181f1f66ce9622acd41ba49bd752
Garrett Gunnell <garrett99@bendbroadband.com>
2018-11-07 21:29:13 -0800

    Implemented Binary Tree constructor.

--- AnimatedTree.pyde
+++ AnimatedTree.pyde
@@ -1,1 +1,11 @@
 
+
+def setup():
+    fullScreen()
+    
+
+def draw():
+    background(255)
+    noStroke()
+    fill(color(200, 100, 50, 150))
+    ellipse(width // 2, height // 2, 100, 100)
---
+++ tree.py
@@ -0,0 +1,8 @@
+class BinaryTree():
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
+        self.parent = None
