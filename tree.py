bca11469630e368ad4d11dc8e64cc5de31018919
Garrett Gunnell <garrett99@bendbroadband.com>
2018-11-07 21:35:10 -0800

    Initial draw function implementation.

--- tree.py
+++ tree.py
@@ -1,8 +1,15 @@
+import random
+
 class BinaryTree():
     
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.color = color(167, 105, 181, 150)
         self.left = None
         self.right = None
         self.parent = None
+        
+    def draw(self):
+        fill(self.color)
+        ellipse(self.x, self.y, 100, 100)
