eaddcab980074b74bd2365adaa1a23598bca72ae
Garrett Gunnell <garrett99@bendbroadband.com>
2018-11-12 00:22:19 -0800

    Rain now a lot more randomized.

--- AnimatedTree.pyde
+++ AnimatedTree.pyde
@@ -1,22 +1,28 @@
 from tree import BinaryTree
 from rain import Rain
 import time
+import random
 
 t = BinaryTree()
-r = Rain(100, 0)
+NUM_RAIN_DROPS = 500
+rain = []
 
 def setup():
     fullScreen()
     t.set_x(width // 2)
     t.set_y(100)
+    for i in range(NUM_RAIN_DROPS):
+        rain.append(Rain(random.uniform(0, width), random.uniform(0, height)))
+    
     
 
 def draw():
-    background(255)
+    background(0)
     noStroke()
     t.draw(time.time())
-    r.move()
-    r.draw()
+    for i in range(NUM_RAIN_DROPS):
+        rain[i].move()
+        rain[i].draw()
     
 def mouseClicked():
     t.insert()
--- rain.py
+++ rain.py
@@ -4,16 +4,29 @@ class Rain():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.angle = random.uniform(-10, 10)
-        self.speed = 2
-        self.color = color(175, 200, 20)
-        self.length_ = 20
-        self.width_ = 1
+        rand = random.randint(0, 1000)
+        if rand > 500:
+            self.angle = 3.05
+        else:
+            self.angle = -3.05
+        self.speed = random.uniform(20, 25)
+        self.color = color(175)
+        self.length_ = random.uniform(30, 50)
+        self.width_ = random.uniform(0.1, 0.15)
         
         
     def reset(self):
         self.y = 0
         self.x = random.uniform(0, width)
+        rand = random.randint(0, 1000)
+        if rand > 500:
+            self.angle = random.uniform(3.0, 3.10)
+        else:
+            self.angle = random.uniform(-3.1, -3.0)
+        self.speed = random.uniform(20, 25)
+        self.color = color(175)
+        self.length_ = random.uniform(30, 50)
+        self.width_ = random.uniform(0.1, 0.15)
         
     def move(self):
         self.y += self.speed
@@ -25,9 +38,11 @@ class Rain():
             self.reset()
             
     def draw(self):
+        noStroke()
         pushMatrix()
         translate(self.x, self.y)
         fill(self.color)
+        rectMode(CENTER)
         rotate(self.angle)
         rect(0, 0, self.width_, self.length_)
         popMatrix()
